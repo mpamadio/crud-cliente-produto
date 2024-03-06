c403ab9a449d274627285a9b10aafef88976f799
[mpamadio] <mpamadio@yahoo.com.br>
2024-03-06 17:37:36 -0300

    Basic ProdutoForm component

---
+++ src/components/ProdutoForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <h2>Cadastro Produto</h2>
+    <form @submit.prevent="submitForm">
+      <label for="nome">Nome:</label>
+      <input type="text" id="nome" v-model="produto.nome" required>
+  
+      <label for="ativo">Ativo:</label>
+      <select id="ativo" v-model="produto.ativo">
+        <option value="Sim">Sim</option>
+        <option value="Não">Não</option>
+      </select>
+  
+      <button type="submit">Salvar</button>
+    </form>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        produto: {
+          nome: '',
+          ativo: 'Sim'
+        }
+      };
+    },
+    methods: {
+      submitForm() {
+        console.log('Dados do produto:', this.produto);
+        this.produto = {
+          nome: '',
+          ativo: 'Sim'
+        };
+      }
+    }
+  };
+  </script>
+  
