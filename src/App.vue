c37de82de87cd461ab9529581c93ce129c36402a
[mpamadio] <mpamadio@yahoo.com.br>
2024-03-06 23:15:38 -0300

    Router configuration and MenuComponent created and implemented in main.js

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <template>
   <div class="mx-auto container">
-    <h1>Gerenciamento de Clientes e Produtos</h1>
-    <ClienteForm />
-    <ClienteList />
-    <ProdutoForm />
-    <ProdutoList />
-    <AssociarProduto />
+    <MenuComponent />
+    <router-view />    
   </div>
 </template>
 
 <script>
-import ClienteForm from './components/ClienteForm.vue';
-import ClienteList from './components/ClienteList.vue'
-import ProdutoForm from './components/ProdutoForm.vue'
-import ProdutoList from './components/ProdutoList.vue'
-import AssociarProduto from './components/AssociarProdutos.vue'
+
+import MenuComponent from './components/MenuComponent.vue'
 
 export default {
   name: 'App',
-  components: {
-    ClienteForm,
-    ClienteList,
-    ProdutoForm,
-    ProdutoList,
-    AssociarProduto,
+  components: {    
+    MenuComponent,
   }
 }
 </script>
